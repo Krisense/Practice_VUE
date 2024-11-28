e325ce00f83888a1cc3ca00c235ef2fb1c277df9
Krisense <kristian.kristian.2014@mail.ru>
2024-11-28 18:35:38 +0300

    Initial commit

---
+++ vue_practice/src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="nav">
+    <div class="nav-in">
+      <div class="logo">Логотип</div>
+      <div class="name">Имя, фамилия</div>
+      <div class="menu-dropdown">MENU</div>
+    </div>
+  </div>
+  <div class="app">
+    <post-list :posts="posts" @remove="removePost" v-if="!isPostsLoading" />
+
+    <div v-else>Идет загрузка...</div>
+    <div class="app__btns">
+      <my-button @click="showDialog"> Новый осмотр </my-button>
+      <my-select />
+    </div>
+
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
+  </div>
+</template>
+
+<script>
+import PostForm from "@/Components/PostForm";
+import PostList from "@/Components/PostList";
+import axios from "axios";
+import MyButton from "@/Components/UI/MyButton.vue";
+
+export default {
+  components: {
+    PostForm,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get(
+          "http://26.181.201.144:8000/patient/get_one/5"
+        );
+        this.posts = response.data;
+      } catch (e) {
+        alert("Ошибка");
+      } finally {
+        this.isPostsLoading = false;
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+}
+
+.app__btns {
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav {
+  width: 100%;
+  height: 50px;
+  background-color: teal;
+  margin-bottom: 10px;
+}
+
+.logo {
+  margin: 15px;
+}
+.nav-in {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.name {
+  margin-right: 15px;
+}
+</style>
---
+++ vue_practice/src/Components/PostItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="post">
+    <div>
+      <div><strong>Название-</strong>{{ post.first_name }}</div>
+      <div><strong>Описание-</strong>{{ post.second_name }}</div>
+    </div>
+    <div class="post__btns">
+      <my-button @click="$emit('remove', post)">Удалить</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+ 
+
+export default {
+ 
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
